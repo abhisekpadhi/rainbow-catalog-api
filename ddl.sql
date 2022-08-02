70de3ba1cb2039b8fdb5d4300f05f75febe5700e
Abhisek Padhi <avicool000@gmail.com>
2022-08-02 13:13:09 +0530

    updated ddl

--- ddl.sql
+++ ddl.sql
@@ -1,11 +1,36 @@
+create table buyerOrder
+(
+    id          bigint auto_increment
+        primary key,
+    orderId     varchar(45)  null,
+    customerId  varchar(45)  null,
+    ctxTxnId    varchar(255) null,
+    createdAt   bigint       null,
+    orderStatus varchar(45)  null,
+    refundTerms varchar(45)  null,
+    ff          text         null,
+    billing     text         null,
+    quote       text         null,
+    items       text         null,
+    extraData   text         null
+);
+
 create table farm
 (
     id           bigint auto_increment
         primary key,
-    farmerId     bigint      null,
-    farmName     varchar(48) null,
-    farmLocation varchar(64) null
+    farmerId     bigint       null,
+    farmName     varchar(48)  null,
+    farmLocation varchar(64)  null,
+    providerId   varchar(45)  null,
+    supportPhone varchar(10)  null,
+    supportEmail varchar(255) null,
+    rating       int          null,
+    extraData    text         null
 );
+
+create fulltext index farmName
+    on farm (farmName);
 
 create table farmInventory
 (
@@ -14,7 +39,8 @@ create table farmInventory
     farmId       bigint      null,
     priceInPaise bigint      null,
     productId    varchar(36) null,
-    qty          int         null
+    qty          int         null,
+    itemId       varchar(45) null
 );
 
 create table farmInventoryLedger
@@ -26,7 +52,8 @@ create table farmInventoryLedger
     qty       int         null,
     op        varchar(32) null,
     opening   int         null,
-    createdAt bigint      null
+    createdAt bigint      null,
+    itemId    varchar(45) null
 );
 
 create table farmPrefs
@@ -46,6 +73,17 @@ create table farmer
     phone      varchar(10) null
 );
 
+create table ondcSubscriberInfo
+(
+    id            bigint auto_increment
+        primary key,
+    subscriberId  varchar(255) null,
+    uniqueKeyId   varchar(255) null,
+    details       text         null,
+    currentActive bit          null,
+    createdAt     bigint       null
+);
+
 create table productCatalog
 (
     id                 bigint auto_increment
@@ -61,5 +99,16 @@ create table productCatalog
     coldChain          varchar(32)   null,
     idealDelTat        varchar(32)   null,
     skuId              varchar(36)   null
+);
+
+create fulltext index variant
+    on productCatalog (variant, productDescription, productName);
+
+create table rating
+(
+    id       bigint auto_increment
+        primary key,
+    ctxTxnId varchar(255) null,
+    payload  text         null
 );
 
